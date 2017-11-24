8f9b5b28917a661f435cf2d64614552b16867ceb
Andrew <andrewaccuardi@gmail.com>
2017-11-23 22:47:52 -0800

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const Layout = (props) => {
-  return (
-    <div>
-      <p>Header</p>
-      {props.children}
-      <p>Footer</p>
-    </div>
-  );
-};
-
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
